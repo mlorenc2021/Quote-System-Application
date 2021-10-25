71ab94d7586b15c9ba96de429c6352242af9a465
Ryan-Jeziorski <59761454+Ryan-Jeziorski@users.noreply.github.com>
2021-10-25 13:06:09 -0500

    Setup (#2)
    * Add gitignore file
    
    * Add package.json for node dependancies
    
    * Add package-lock.json
    
    * Fix typo in .gitignore, add node_modules to gitignore
    
    * Update readme
    
    * Add dependancies for express
    
    * Add dev-dependancy for nodemon
    
    * Add start scipt to run app with nodemon
    
    * Create app.js file
    
    * Created basic routing using Node.js and Expresss
    
    * Add public directory for static content, create index.html
    
    * Add README.MD to public directory
    
    * Change html title to Home page

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Tasks 10/21/20 to 10/28/20
 ----------------------------------
-- Create GitHub Repo for project
-- Get everyone access to GitHub repo
+- ~~Create GitHub Repo for project~~
+- ~~Get everyone access to GitHub repo~~
 - Create homepage with node.js
     - Create menu bar to link to other pages
 - Connect to Ege Database
---
+++ app.js
@@ -0,0 +1,22 @@
+//Import express module
+const express = require('express');
+//Import path module
+const path = require('path');
+
+//invoke express function to create server
+const app = express();
+
+// Import static elements from public directory
+app.use(express.static('./public'));
+
+// If user is attempting to access a resource that doesn't exist
+app.all('*', (req,res) => {
+    res.status(404).send('error 404');
+});
+
+
+//Set server to lisen on port 5000
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+

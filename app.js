f2eb74e14236f5f4eaa57a5203042355c68d64b8
Ryan-Jeziorski <59761454+Ryan-Jeziorski@users.noreply.github.com>
2021-11-07 17:39:20 -0600

    Contact (#7)
    * Add contact page
    
    * Added nodemailer dependency
    
    * Added contact html and styling
    
    * Added contact.js
    
    * Email backend and frontend
    
    * Subbed email creds for env vars
    
    Co-authored-by: mlorenc2021 <niustudent2021@gmail.com>

--- app.js
+++ app.js
@@ -8,8 +8,49 @@ require('dotenv').config();
 //invoke express function to create server
 const app = express();
 
+const nodemailer = require('nodemailer');
+
 // Import static elements from public directory
 app.use(express.static('./views'));
+app.use(express.json())
+
+//Middleware
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/contact.html')
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body)
+
+    //pass email details
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.PROJECTEMAIL,
+            pass: process.env.PROJECTPASS
+        }
+    })
+
+    //pass submission fields to email receiver
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.PROJECTEMAIL,
+        subject: `Message from' ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    //send error or success to frontend upon message submission
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+
+})
 
 // If user is attempting to access a resource that doesn't exist
 app.all('*', (req,res) => {

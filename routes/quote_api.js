a8c5be5cbbf9c171e96cd52061beaddad82cfbf9
ryan <rjeziorski@outlook.com>
2021-11-24 19:26:55 -0600

    Add functionality to promote quotes, basic error checking
    (cherry picked from commit fc8d42cf4f3b3e1a8a9da98f4a9574cf6c2be7db)

--- routes/quote_api.js
+++ routes/quote_api.js
@@ -11,4 +11,13 @@ router.get('/', quote_controller.quote_get_all);
 // API to get an quote based on aployee user_name
 router.get('/:user_name', quote_controller.quote_get_all_for_user);
 
+// API to finalize a quote
+router.post('/finalize/:id', quote_controller.finalize_quote);
+
+// API to finalize a quote
+router.post('/sanction/:id', quote_controller.sanction_quote);
+
+// API to finalize a quote
+router.post('/purchase/:id', quote_controller.purchase_order);
+
 module.exports = router;

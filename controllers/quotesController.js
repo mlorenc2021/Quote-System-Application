b8a6455cba90b7931ca03aff9266cd4110d6f25e
ryan <rjeziorski@outlook.com>
2021-11-26 17:40:01 -0600

    Add skeleton for querying for reviewing quotes

--- controllers/quotesController.js
+++ controllers/quotesController.js
@@ -1,4 +1,6 @@
 const { quote } = require('../db/models');
+const { Op } = require('sequelize');
+const sequelize = require('sequelize');
 
 //apis for quotes
 exports.quote_create = async function(req,res) {
@@ -48,6 +50,52 @@ exports.quote_get_all_for_user = async function(req,res) {
     const user_name = req.params.user_name; //store username param in user_name
     try {
         const qte = await quote.findAll({where: {user_name}});
+        return res.send(qte);
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send({error: 'Something went wrong'}, err);
+    }
+};
+
+// API for reviewing quotes based on custom query
+exports.review_quotes = async function(req,res) {
+    const start_date = req.body.start_date;
+    const end_date = req.body.end_date;
+    const status = req.body.status;
+    const user_name = req.body.user_name;
+    const customer = req.body.customer;
+
+    // const body = req.body;
+    // console.log(body)
+
+    let query_map = new Map();
+
+    if(start_date != '') {
+        query_map.set('start_date', start_date);
+    }
+
+    if(end_date != '') {
+        query_map.set('start_date', start_date);
+    }
+
+    if(user_name != '') {
+        query_map.set('user_name', user_name);
+    }
+
+    if(status != '') {
+        query_map.set('status', status);
+    }
+
+    if(customer != '') {
+        query_map.set('customer', customer);
+    }
+
+    const query_object = Object.fromEntries(query_map);
+
+    try {
+        const qte = await quote.findAll({
+            where: query_object
+        })
         return res.send(qte);
     } catch(err) {
         console.log(err);

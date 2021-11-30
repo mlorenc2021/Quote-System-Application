0390bf96c289dc4636287f2de928098abf88b451
Adalberto Orozco <z1885975@students.niu.edu>
2021-11-29 20:21:30 -0600

    finalize edit, add route for update

--- routes/employee_api.js
+++ routes/employee_api.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const employee_controller = require('../controllers/employeeController');
 
-// API to create an employee
-router.post('/', employee_controller.employee_create);
-
 // API to get all employees from employees table
 router.get('/', employee_controller.employee_get_all);
 
 // API to get an employee based on user_name
 router.get('/:user_name', employee_controller.employee_get_one);
+
+// API to create an employee
+router.post('/', employee_controller.employee_create);
+
+// API to create an employee
+router.post('/update', employee_controller.employee_update);
 
 module.exports = router;

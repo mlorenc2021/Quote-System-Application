8ce5ce14f5d80f6233c0135f9b0a4a6839610247
mlorenc2021 <niustudent2021@gmail.com>
2021-11-30 11:58:34 -0600

    passed necesary variables into sanction and update quotes

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -33,16 +33,29 @@ exports.finalize_quote = async function (req, res) {
 };
 
 
+
+
 //manager dashboard and interfaces
 exports.manager_dashboard = async function(req,res) {
     await res.render('./manager/manager_dashboard.ejs');
 };
 
 exports.update_quote = async function (req, res) {
-    await res.render('./manager/update_quote.ejs');
+    qte = await quote.quote_get_one(req, res);
+    cust = await customer.customer_get_all();
+    console.log('What is quote.id?:', qte.id);
+    line_items = await quote.get_line_items(qte.id);
+    secret_notes = await quote.get_secret(qte.id);
+    await res.render('./manager/update_quote.ejs', {
+        qte: qte,
+        line_items: line_items,
+        secret_notes: secret_notes,
+        cust: cust
+    });
 };
 
 exports.sanction_quote = async function (req, res) {
+    qte = await quote.quote_get_all_by_status('finalized');
     await res.render('./manager/sanction_quote.ejs');
 };
 

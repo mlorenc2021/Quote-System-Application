f5556bb8c5f11095223eb29f292ef55be46fc37d
ryan <rjeziorski@outlook.com>
2021-12-01 08:05:10 -0600

    Upated seeders

--- db/seeders/20211115031905-quotes-demo.js
+++ db/seeders/20211115031905-quotes-demo.js
@@ -7,7 +7,7 @@ module.exports = {
         total: 120,
         status: 'draft',
         cust_email: 'customer1@customerdomain.com',
-        customer: 'Test customer 1',
+        customer: 'Saveley & Henriot, Co.',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -16,7 +16,7 @@ module.exports = {
         total: 190,
         status: 'draft',
         cust_email: 'customer2@customerdomain.com',
-        customer: 'Test customer 2',
+        customer: 'UK Collectables, Ltd.',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -25,7 +25,7 @@ module.exports = {
         total: 1120,
         status: 'draft',
         cust_email: 'customer3@customerdomain.com',
-        customer: 'Test customer 3',
+        customer: 'Reams Paper and Sausage',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -34,7 +34,79 @@ module.exports = {
         total: 10000,
         status: 'draft',
         cust_email: 'customer4@customerdomain.com',
-        customer: 'Test customer 4',
+        customer: 'Asus Tech',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 120,
+        status: 'draft',
+        cust_email: 'customer1@customerdomain.com',
+        customer: 'Saveley & Henriot, Co.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 190,
+        status: 'draft',
+        cust_email: 'customer2@customerdomain.com',
+        customer: 'UK Collectables, Ltd.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 1120,
+        status: 'draft',
+        cust_email: 'customer3@customerdomain.com',
+        customer: 'Reams Paper and Sausage',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 10000,
+        status: 'draft',
+        cust_email: 'customer4@customerdomain.com',
+        customer: 'Toys4GrownUps.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 190,
+        status: 'sanctioned',
+        cust_email: 'customer2@customerdomain.com',
+        customer: 'UK Collectables, Ltd.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 1120,
+        status: 'sanctioned',
+        cust_email: 'customer3@customerdomain.com',
+        customer: 'Reams Paper and Sausage',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 190,
+        status: 'sanctioned',
+        cust_email: 'customer2@customerdomain.com',
+        customer: 'UK Collectables, Ltd.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_name: 'sales',
+        total: 1120,
+        status: 'finalized',
+        cust_email: 'customer3@customerdomain.com',
+        customer: 'Reams Paper and Sausage',
         createdAt: new Date(),
         updatedAt: new Date()
       }
--- db/seeders/20211128081024-secret-notes-demo.js
+++ db/seeders/20211128081024-secret-notes-demo.js
@@ -10,44 +10,68 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        quote_id: 1,
+        quote_id: 2,
         note: 'As a manager I can approve this discount!',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        quote_id: 2,
+        quote_id: 3,
         note: 'Customer wishes for a start time before our operating hours',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        quote_id: 2,
+        quote_id: 4,
         note: 'We are a union shop, they will have to wait',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        quote_id: 3,
+        quote_id: 5,
         note: 'Customers card was decline last time, watch out!',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        quote_id: 4,
+        quote_id: 6,
         note: 'Door code is 555',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        quote_id: 4,
+        quote_id: 7,
         note: 'Key is under floor matt',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        quote_id: 4,
+        quote_id: 8,
         note: 'Do not give this customer a discount, they have been repeatedly told they will not get one',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        quote_id: 9,
+        note: 'Customer wants to know if you have extra paint',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        quote_id: 10,
+        note: 'Customer wishes for an 11pm start time',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        quote_id: 11,
+        note: 'Customer was irate that the last time we were out there we didn\'t fix the problem',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        quote_id: 12,
+        note: 'Customer is a bakery said lunch is on them',
         createdAt: new Date(),
         updatedAt: new Date()
       },
